be0fc0cd451993dd48f64be396af2202f8e5502d
Mrzzchao <mrzzhichao@gmail.com>
2021-03-31 00:27:57 +0800

    feat: update

---
+++ flushGitCommit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="zhichazhang@tencent.com"
+CORRECT_NAME="Mrzzchao"
+CORRECT_EMAIL="mrzzhichao@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$CORRECT_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$CORRECT_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
---
+++ modifyGitConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git remote add origin "https://github.com/Mrzzchao/learn-react-source.git"
+
+
+# 设置项目邮箱.
+git config user.email "mrzzhichao@gmail.com"
+ 
+# 设置项目用户名
+git config user.name "Mrzzchao" 
